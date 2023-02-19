1b458462c3f8b1e7c4b9e1d7bbbfe667bc3e4b23
Theo Rode <theorodester@gmail.com>
2023-02-18 16:59:20 -0800

    moved to saving commands on enter rather than on change or whatever it was

---
+++ src/command_parsing.rs
@@ -0,0 +1,13 @@
+pub const SUPPORTED_COMMANDS: &[&str] = &["help"];
+
+/// Checks to see if the supplied command is valid
+pub fn valid_command(command: String) -> bool {
+    let command: Vec<&str> = command.split(' ').collect(); // split command at whitespace for parsing
+
+    // if SUPPORTED_COMMANDS.contains(&command[0]) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    SUPPORTED_COMMANDS.contains(&command[0])
+}
--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,29 @@ use stylist::yew::{use_style, Global};
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
+mod command_parsing;
+
+use command_parsing::valid_command;
+
 #[derive(Properties, PartialEq)]
 pub struct CommandEnterLineProps {
-    pub on_command_entry: Callback<web_sys::Event>,
+    pub on_input: Callback<web_sys::InputEvent>,
+    pub node_ref: NodeRef,
+    pub on_key_enter: Callback<KeyboardEvent>,
 }
 
 /// Function component for text extry line where user puts in commands
 #[function_component(CommandEnterLine)]
 fn command_enter_line(props: &CommandEnterLineProps) -> Html {
-    let input_node_ref = use_node_ref();
+    let input_node_ref = props.node_ref.clone();
 
     let input_value_handle = use_state(String::default);
     let input_value = (*input_value_handle).clone();
 
-    let oninput = {
-        let input_node_ref = input_node_ref.clone();
+    let oninput = props.on_input.reform(move |e| e);
+    let onkeyenter = props.on_key_enter.reform(move |e| e);
 
-        Callback::from(move |_| {
-            let input = input_node_ref.cast::<HtmlInputElement>();
-
-            if let Some(input) = input {
-                input_value_handle.set(input.value());
-            }
-        })
-    };
-
-    let onchange = props.on_command_entry.reform(move |e| e);
-
-    let style_main = use_style!("color: var(--text-color-fifth);");
+    let style_main = use_style!("color: var(--text-color-fifth); margin: 0px;");
     let style1 = use_style!("color: var(--text-color-third);");
     let style2 = use_style!("color: var(--text-color-secondary);");
     let style3 = use_style!("color: var(--text-color-quad);");
@@ -52,16 +47,18 @@ fn command_enter_line(props: &CommandEnterLineProps) -> Html {
                 <span class={style3}>{"~"}</span> // temp, change to dir later
                 {"$"}
             </p>
-            <label for="command-input">
-                <input ref={input_node_ref}
-                    oninput={oninput}
-                    onchange={onchange}
-                    id="command-input"
-                    type="text"
-                    class="command-input"
-                />
-            </label>
-            <p> {input_value.clone()} </p>
+            <div class="command-input">
+                <label for="command-input">
+                    <input ref={input_node_ref}
+                        oninput={oninput}
+                        onkeyup={onkeyenter}
+                        id="command-input"
+                        type="text"
+                        class="command-input"
+                        maxlength="100"
+                    />
+                </label>
+            </div>
         </div>
     )
 }
@@ -69,22 +66,34 @@ fn command_enter_line(props: &CommandEnterLineProps) -> Html {
 #[function_component(App)]
 fn app() -> Html {
     let last_command_handle = use_state(String::default);
-    let command_history_handle = use_mut_ref(Vec::new); // store history of commands
+    let command_history_handle = use_mut_ref(Vec::<String>::new); // store history of commands
     let last_command = (*last_command_handle).clone();
     let command_history = (*command_history_handle).clone();
+    let command_node_ref = use_node_ref();
 
-    let on_command_entry: Callback<web_sys::Event> = {
+    let oncommandinput = {
+        let command_node_ref = command_node_ref.clone();
         let last_command_handle = last_command_handle.clone();
 
-        Callback::from(move |command: Event| {
-            let input = command.target_dyn_into::<HtmlInputElement>();
+        Callback::from(move |_| {
+            let input = command_node_ref.cast::<HtmlInputElement>();
 
             if let Some(input) = input {
                 last_command_handle.set(input.value());
+            }
+        })
+    };
 
-                (*command_history_handle.borrow_mut()).push(input.value());
-
-                input.set_value("");
+    let onkeyenter = {
+        Callback::from(move |e: KeyboardEvent| {
+            if e.key() == "Enter" {
+                let target = e.target_dyn_into::<HtmlInputElement>();
+                if let Some(target) = target {
+                    let input = target.value();
+                    target.set_value("");
+                    (*command_history_handle.borrow_mut()).push(input.clone());
+                    last_command_handle.set(input);
+                }
             }
         })
     };
@@ -98,6 +107,7 @@ fn app() -> Html {
                 --text-color-third: rgb(78, 55, 135);
                 --text-color-quad: rgb(43, 122, 99);
                 --text-color-fifth: rgb(89, 98, 107);
+                --text-color-incorrect: rgb(156, 31, 31);
                 --background-color: rgb(36,37,38);
             }
 
@@ -126,8 +136,21 @@ fn app() -> Html {
                 justify-content: flex-start;
             } 
 
-            div.command-single=line {
+            div.command-single-line {
+                display: flex;
+                flex-direction: row;
+                justify-content: flex-start;
+            }
 
+            div.command-input {
+                flex-direction: column;
+                justify-content: center;
+                margin-left: 5px;
+                width: 100%;
+            }
+
+            input.command-input {
+                width: 100%;
             }
 
             input.command-input {
@@ -142,7 +165,11 @@ fn app() -> Html {
         ")}/>
             <div class="main-screen">
                 <h1>{ "Hello World!" }</h1>
-                <CommandEnterLine {on_command_entry}/>
+                <CommandEnterLine
+                    on_input={oncommandinput}
+                    node_ref = {command_node_ref}
+                    on_key_enter = {onkeyenter}
+                />
                 <p> {command_history.borrow().clone()}</p>
             </div>
         </>
